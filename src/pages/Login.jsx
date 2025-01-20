c064cd7badddf89ad5c62c38818c203b00d461f2
Obinna Anthony Dimojiaku <dimojiakuo@accessbankplc.com>
2025-01-20 01:49:49 +0100

    login mobile fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -62,109 +62,8 @@ const Login = () => {
   };
 
   return (
-    // <Box w="100vw" h="100vh">
-    //   <HStack w="100%" h="100%">
-    //     <VStack w="60%" h="100%" alignItems="center" justifyContent="center">
-    //       <form onSubmit={handleSubmit} style={{ width: "100%" }}>
-    //         <VStack
-    //           borderRadius="20px"
-    //           p="20px 60px"
-    //           boxShadow="4px 3px 5px 2px rgba(0,0,0,0.75)"
-    //           alignItems="flex-start"
-    //           gap="1.5rem"
-    //         >
-    //           <HStack>
-    //             <ChevronLeft />
-    //             <Text>LOGO</Text>
-    //           </HStack>
-
-    //           <Text fontSize="36px" fontWeight="600" mb="1rem">
-    //             Welcome Back !
-    //           </Text>
-
-    //           <VStack w="100%" alignItems="flex-start">
-    //             <Field label="Email">
-    //               <Input
-    //                 placeholder="me@example.com"
-    //                 w="25rem"
-    //                 bg="#32ffff0a"
-    //                 color="#101828"
-    //                 fontWeight="600"
-    //                 name="username"
-    //                 value={credentials.username}
-    //                 onChange={handleChange}
-    //                 required
-    //               />
-    //             </Field>
-    //             <Field label="Password">
-    //               <Input
-    //                 placeholder="Enter your password"
-    //                 type="password"
-    //                 w="25rem"
-    //                 bg="#32ffff0a"
-    //                 color="#101828"
-    //                 fontWeight="600"
-    //                 name="password"
-    //                 value={credentials.password}
-    //                 onChange={handleChange}
-    //                 required
-    //               />
-    //             </Field>
-    //             <HStack w="100%" justifyContent="flex-end">
-    //               <Link to="forgot-password">
-    //                 <Text fontSize="12px" _hover={{ transform: "scale(1.1)" }}>
-    //                   Forgot password?
-    //                 </Text>
-    //               </Link>
-    //             </HStack>
-    //           </VStack>
-
-    //           <Button
-    //             w="100%"
-    //             bg="#003737"
-    //             _hover={{ bg: "#000", transform: "scale(1.02)" }}
-    //             type="submit"
-    //             isLoading={isLoading}
-    //             loadingText="Logging in..."
-    //           >
-    //             Log in
-    //           </Button>
-
-    //           <HStack
-    //             gap="0.3rem"
-    //             w="100%"
-    //             justifyContent="center"
-    //             fontSize="14px"
-    //           >
-    //             <Text>I don't have an account?</Text>
-    //             <Link to="register">
-    //               <Text
-    //                 fontSize="12px"
-    //                 fontWeight="600"
-    //                 color="#003737"
-    //                 _hover={{ transform: "scale(1.1)" }}
-    //               >
-    //                 Sign Up
-    //               </Text>
-    //             </Link>
-    //           </HStack>
-    //         </VStack>
-    //       </form>
-    //     </VStack>
-    //     <Box w="40%" h="100%" bg="#003737">
-    //       <Image
-    //         src="/images/Group 2014.svg"
-    //         alt="shopper"
-    //         w="440px"
-    //         position="absolute"
-    //         transform="translateX(-160px)"
-    //         bottom="-3px"
-    //       />
-    //     </Box>
-    //   </HStack>
-    // </Box>
     <>
-      <Box w="100vw" h="100vh">
+      <Box display={{ md: "flex", base: "none" }} w="100vw" h="100vh">
         <HStack w="100%" h="100%">
           <VStack w="60%" h="100%" alignItems="center" justifyContent="center">
             <VStack
@@ -276,6 +175,117 @@ const Login = () => {
             />
           </Box>
         </HStack>
+      </Box>
+
+      <Box display={{ md: "none", base: "flex" }} w="100dvw" h="100dvh">
+        <VStack alignItems="center" px="2rem" pt="10rem" w="100%" h="100%">
+          <VStack
+            w="100%"
+            borderRadius="20px"
+            // p="20px 60px"
+            p="2rem"
+            // boxShadow="0px 4px 4px 0px rgba(0, 0, 0, 0.25)"
+            boxShadow="4px 3px 5px 2px rgba(0,0,0,0.75)"
+            alignItems="flex-start"
+            gap="1.5rem"
+          >
+            <HStack>
+              <ChevronLeft />
+              <Image src="/images/logo.png" alt="logo" w="5rem" />
+            </HStack>
+
+            <Text fontSize="1.4rem" fontWeight="600" mb="1rem">
+              Welcome Back !
+            </Text>
+
+            <form onSubmit={handleSubmit} style={{ width: "100%" }}>
+              <VStack w="100%" alignItems="flex-start">
+                <Field
+                  label="Email"
+                  // invalid
+                  // errorText="This is an error text"
+                >
+                  <Input
+                    placeholder="me@example.com"
+                    w="100%"
+                    bg="#32ffff0a"
+                    color="#101828"
+                    fontWeight="600"
+                    name="email"
+                    value={credentials.email}
+                    onChange={handleChange}
+                    required
+                  />
+                </Field>
+                <Field
+                  label="Password"
+                  // invalid
+                  // errorText="This is an error text"
+                >
+                  <Input
+                    placeholder="Enter your password"
+                    type="password"
+                    w="100%"
+                    bg="#32ffff0a"
+                    color="#101828"
+                    fontWeight="600"
+                    name="password"
+                    value={credentials.password}
+                    onChange={handleChange}
+                  />
+                </Field>
+                <HStack w="100%" justifyContent="flex-end">
+                  <Link to="forgot-password">
+                    <Text fontSize="12px" _hover={{ transform: "scale(1.1)" }}>
+                      Forgot password?
+                    </Text>
+                  </Link>
+                </HStack>
+              </VStack>
+
+              <Button
+                w="100%"
+                bg="#003737"
+                _hover={{ bg: "#000", transform: "scale(1.02)" }}
+                // onClick={handleLogin}
+                type="submit"
+                loading={status === "loading"}
+                loadingText="Logging in..."
+              >
+                Log in
+              </Button>
+            </form>
+
+            <HStack
+              gap="0.3rem"
+              w="100%"
+              justifyContent="center"
+              fontSize="14px"
+            >
+              <Text>I don't have an account?</Text>
+              <Link to="register">
+                <Text
+                  fontSize="12px"
+                  fontWeight="600"
+                  color="#003737"
+                  _hover={{ transform: "scale(1.1)" }}
+                >
+                  Sign Up
+                </Text>
+              </Link>
+            </HStack>
+            <Box w="40%" h="100%" bg="#003737">
+              <Image
+                src="/images/Group 2014.svg"
+                alt="shopper"
+                w="440px"
+                position="absolute"
+                transform="translateX(30px)"
+                bottom="-3px"
+              />
+            </Box>
+          </VStack>
+        </VStack>
       </Box>
     </>
   );
